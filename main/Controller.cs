c63acee3d301d0e6657e5a440a374d358347fbd6
Michael Voskanyan <michael.voskanyan@icloud.com>
2023-11-14 16:33:58 -0800

    Began new vehicle physics engine

---
+++ main/Controller.cs
@@ -0,0 +1,50 @@
+using Godot;
+using Array = Godot.Collections.Array;
+using System;
+
+public partial class Controller : Node
+{
+    /// <summary>
+    /// Settings to check player assists and controller type
+    /// </summary> 
+    [ExportCategory("Player Assists")]
+    [Export] public bool AutomaticOnly = false;
+    [Export] public bool UseController = false;
+    [Export] public bool UseWheel = true;
+    [Export] public bool ABS = false;
+    [Export] public bool TC = false;
+    [Export] public bool SimulationSteering = true;
+    [ExportCategory("Controller Settings")]
+    [Export] public float ThrottleDeadzoneIn = 0.05f;
+    [Export] public float ThrottleDeadzoneOut = 0.05f;
+    [Export] public float BrakeDeadzoneIn = 0.05f;
+    [Export] public float BrakeDeadzoneOut = 0.05f;
+    [Export] public float ClutchDeadzoneIn = 0.05f;
+    [Export] public float ClutchDeadzoneOut = 0.05f;
+
+    /* Input values */
+    public float throttle;
+    public float brake;
+    public float clutch;
+    public float steering;
+
+    public void LogiWheelInit() {
+        // Implement logitech wheel initialization code here
+    }
+
+    public override void _Process(double delta)
+    
+    {
+        base._Process(delta);
+        throttle = Input.GetActionRawStrength("throttle");
+        brake = Input.GetActionRawStrength("brake");
+
+        /* Scaling inputs with deadzones */
+        throttle = (throttle - ThrottleDeadzoneIn) / ( (1 - ThrottleDeadzoneOut) - ThrottleDeadzoneIn );
+        brake = (brake - ThrottleDeadzoneIn) / ( (1 - BrakeDeadzoneOut) - BrakeDeadzoneIn );
+                
+    }
+
+
+
+}
---
+++ main/Wheel.cs
@@ -0,0 +1,35 @@
+using Godot;
+using Dictionary = Godot.Collections.Dictionary;
+using System;
+
+public partial class Wheel : RayCast3D
+{
+    public enum TireType { street_old, street, sport, sport_wet, race, race_wet };
+    
+    /* Tire Data */
+    [ExportCategory("Tire Data")]
+    [Export] public float tire_width = 225;
+    [Export] public float tire_aspect = 45;
+    [Export] public float rim = 17;
+    [Export] public TireType tire_type = TireType.street;
+
+    [ExportCategory("Suspension Configuration")]
+    [Export] Wheel anti_roll_bar_connection;
+    [Export] float anti_roll_rate;
+    [Export] float spring_rate;
+    [Export] float bump;
+    [Export] float rebound;
+    
+    [ExportCategory("Differential Configuration")]
+    [Export] Wheel differential_connection;
+    [Export] float diff_accel_perc;
+    [Export] float diff_coast_perc;
+
+    public float TireDiameter() {
+        float diameter = 2 * (tire_width * (tire_aspect / 100f));
+        return diameter + (25.4f * rim);
+    }
+
+
+
+}
